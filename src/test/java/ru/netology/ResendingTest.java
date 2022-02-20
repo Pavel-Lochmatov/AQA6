6b8fa590bad982e2143396698c50ff04c91af35d
Pavel-Lokhmatov <hlest.ivsvnyby@gmail.com>
2022-02-20 12:33:57 +0400

    add allure

--- src/test/java/ru/netology/ResendingTest.java
+++ src/test/java/ru/netology/ResendingTest.java
@@ -1,22 +1,39 @@
 package ru.netology;
 
 import static com.codeborne.selenide.Selectors.withText;
+
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.Condition;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static ru.netology.DataGenerator.*;
+
 import java.time.Duration;
+
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+import io.qameta.allure.selenide.AllureSelenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 
 public class ResendingTest {
 
     private UserInfo user;
 
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+
     @BeforeEach
-    void setUpAll() {
+    void setUp() {
 
         user = getUserInfo();
     }
@@ -50,4 +67,30 @@ public class ResendingTest {
         $("[data-test-id=success-notification]").shouldHave(text("Встреча успешно запланирована на " + generateDate((5))));
 
     }
+
+    @Test
+    void shouldResendFormFail() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=city] input").setValue(user.getCity());
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(generateDate(4));
+        form.$("[data-test-id=name] input").setValue(user.getName());
+        form.$("[data-test-id=phone] input").setValue(user.getPhone());
+        form.$("[data-test-id=agreement]").click();
+        $$("button").find(Condition.exactText("Запланировать")).click();
+        $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id=success-notification]").shouldHave(text("Встреча успешно запланирована на " + generateDate((4))));
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        form.$("[data-test-id=date] input").setValue(generateDate(5));
+        $$("button").find(Condition.exactText("Запланировать")).click();
+        $(withText("Необходимо подтверждение")).shouldBe(Condition.visible, Duration.ofSeconds(3));
+        $$("button").find(Condition.exactText("Перепланировать")).click();
+        $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(3));
+        $("[data-test-id=success-notification]").shouldHave(text("Встреча успешно 777запланирована на " + generateDate((5))));
+
+    }
+
 }
